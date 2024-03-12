707b80bd38853da38c5ff5904f49a54e35c1c7f6
pvshake <pvshakeu@gmail.com>
2024-03-12 12:15:32 -0300

    melhorando metadata para SEO, favicon e loading skeleton

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,15 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Performance Pvshake IMDB",
   description: "Performance Pvshake IMDB",
+  other: {
+    "theme-color": "#0d1117",
+    "color-scheme": "dark only",
+    "twitter:image": "/homepage-thumbnail.png",
+    "twitter:card": "summary_large_image",
+    "og:url": "performance-imdb",
+    "og:image": "/homepage-thumbnail.png",
+    "og:type": "website",
+  },
 };
 
 export default function RootLayout({
